9ea7f818a7db20db34f4a3922bdc508cb0dae390
Jean Baptiste Favre <jean-baptiste.favre@blablacar.com>
2014-10-22 12:57:37 +0200

    Imported Upstream version 2.4.1

--- src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
+++ src/zabbix_java/src/com/zabbix/gateway/GeneralInformation.java
@@ -22,9 +22,9 @@ package com.zabbix.gateway;
 class GeneralInformation
 {
 	public static final String APPLICATION_NAME = "Zabbix Java Gateway";
-	public static final String REVISION_DATE = "11 September 2014";
-	public static final String REVISION = "48953";
-	public static final String VERSION = "2.4.0";
+	public static final String REVISION_DATE = "8 October 2014";
+	public static final String REVISION = "49643";
+	public static final String VERSION = "2.4.1";
 
 	public static void printVersion()
 	{
